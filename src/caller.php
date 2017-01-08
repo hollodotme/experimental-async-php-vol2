9e4e25f381372e9ae65ddc3ad26c6a5619effca9
Holger Woltersdorf <hw@hollo.me>
2017-01-08 21:37:43 +0100

    Adds comments to caller

--- src/caller.php
+++ src/caller.php
@@ -10,15 +10,19 @@ require(__DIR__ . '/../vendor/autoload.php');
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+# Connect and retrieve a channel
 $connection = new AMQPStreamConnection( 'localhost', 5672, 'guest', 'guest' );
 $channel    = $connection->channel();
 
+# Make sure the queue 'commands' exist
 $channel->queue_declare( 'commands' );
 
-$message = new AMQPMessage( 'Do something' );
+# Create and send the message
+$message = new AMQPMessage( json_encode( [ 'timestamp' => date( 'c' ) ], JSON_PRETTY_PRINT ) );
 $channel->basic_publish( $message, '', 'commands' );
 
-echo " [x] Sent 'Do something' to 'commands'\n";
+echo " [x] Message sent.\n";
 
+# Close channel and connection
 $channel->close();
 $connection->close();
