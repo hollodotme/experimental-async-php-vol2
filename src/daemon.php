dcd218b1a496ab5a9af25ad9239316f68007ff4a
Holger Woltersdorf <hw@hollo.me>
2017-01-08 22:49:13 +0100

    Adds comments to daemon

--- src/daemon.php
+++ src/daemon.php
@@ -5,26 +5,59 @@
 
 namespace hollodotme\AsyncPhp;
 
+use hollodotme\FastCGI\Client;
+use hollodotme\FastCGI\SocketConnections\UnixDomainSocket;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 require(__DIR__ . '/../vendor/autoload.php');
 
+# Connect to the same RabbitMP instance and get a channel
 $connection = new AMQPStreamConnection( 'localhost', 5672, 'guest', 'guest' );
 $channel    = $connection->channel();
 
+# Make sure the queue "commands" exists
 $channel->queue_declare( 'commands' );
 
-echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
+# Prepare the Fast CGI Client
+$unixDomainSocket = new UnixDomainSocket( 'unix:///var/run/php/php7.1-fpm-commands.sock' );
+$fpmClient        = new Client( $unixDomainSocket );
 
-$callback = function ( AMQPMessage $msg )
+# Define a callback function that is invoked whenever a message is consumed
+$callback = function ( AMQPMessage $message ) use ( $fpmClient )
 {
-	var_dump( $msg );
-	echo " [x] Received ", $msg->getBody(), "\n";
+	echo " [x] Received:\n", $message->getBody(), "\n";
+
+	# Decode the json message and encode it for sending to php-fpm
+	$messageArray = json_decode( $message->getBody() );
+	$body         = http_build_query( $messageArray );
+
+	# Send an async request to php-fpm pool and receive a process ID
+	$processId = $fpmClient->sendAsyncRequest(
+		[
+			'GATEWAY_INTERFACE' => 'FastCGI/1.0',
+			'REQUEST_METHOD'    => 'POST',
+			'SCRIPT_FILENAME'   => '/vagrant/src/worker.php',
+			'SERVER_SOFTWARE'   => 'php/fcgiclient',
+			'REMOTE_ADDR'       => '127.0.0.1',
+			'REMOTE_PORT'       => '9985',
+			'SERVER_ADDR'       => '127.0.0.1',
+			'SERVER_PORT'       => '80',
+			'SERVER_NAME'       => 'myServer',
+			'SERVER_PROTOCOL'   => 'HTTP/1.1',
+			'CONTENT_TYPE'      => 'application/x-www-form-urlencoded',
+			'CONTENT_LENGTH'    => mb_strlen( $body ),
+		],
+		$body
+	);
+
+	echo "Spawned process with ID: {$processId}\n";
 };
 
+# Request consumption for queue "commands" using the defined callback function
 $channel->basic_consume( 'commands', '', false, true, false, false, $callback );
 
+# Wait to finish execution as long as the channel has callbacks
 while ( count( $channel->callbacks ) )
 {
 	$channel->wait();
